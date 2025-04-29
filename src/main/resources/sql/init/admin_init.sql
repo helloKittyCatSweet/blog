16772d47572ba10b75814121db3c0c42ee71e35e
helloKittyCatSweet <1448185924@qq.com>
2025-04-29 17:36:21 +0800

    修改了前端的背景展示，修改了user_role的一个外键依赖

--- src/main/resources/sql/init/admin_init.sql
+++ src/main/resources/sql/init/admin_init.sql
@@ -1,4 +1,4 @@
 insert into fs_users (username, password, email) values
 ('admin', 'eyJhbGciOiJIUzI1NiJ9.eyJ1dWlkIjoiYWM3NzhmM2EtZjdiZS00NzAyLWExZTgtYmEwMzQzMzg5MDEwIiwic3ViIjoiaGVsbG9LaXR0eUNhdFN3ZWV0IiwiaWF0IjoxNzQ1NTg0OTg3LCJleHAiOjE3NDU1ODg1ODd9.T38lhnmvgXF8FuKXprvlnUPbhP00ZUtzgAHLI3G_dGU', 'admin@admin.com');
 insert into fs_user_roles (user_id, role_id) values (1, 1);
-insert into fs_user_roles (user_id, role_id) values (1, 9);
+insert into fs_user_roles (user_id, role_id) values (1, 8);
