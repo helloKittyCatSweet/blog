35b5186cd8bdb31b88aedda3dc8f1afade7a45c0
helloKittyCatSweet <1448185924@qq.com>
2025-04-30 20:34:43 +0800

    修复不能创建tag的问题

--- src/main/java/com/kitty/blog/domain/model/tag/TagDeserializer.java
+++ src/main/java/com/kitty/blog/domain/model/tag/TagDeserializer.java
@@ -19,21 +19,31 @@ public class TagDeserializer extends JsonDeserializer<Tag> {
             tag.setTagId(node.asInt());
         } else {
             // 否则解析完整的Tag对象
-            tag.setTagId(node.get("tagId").asInt());
             if (node.has("name")) {
                 tag.setName(node.get("name").asText());
             }
+            if (node.has("adminWeight")) {
+                tag.setAdminWeight(node.get("adminWeight").asInt());
+            }
+
+            // 其他字段设置默认值
+            if (node.has("tagId")) {
+                tag.setTagId(node.get("tagId").asInt());
+            }
             if (node.has("useCount")) {
                 tag.setUseCount(node.get("useCount").asInt());
+            } else {
+                tag.setUseCount(0);
             }
             if (node.has("clickCount")) {
                 tag.setClickCount(node.get("clickCount").asInt());
+            } else {
+                tag.setClickCount(0);
             }
             if (node.has("weight")) {
                 tag.setWeight(node.get("weight").asInt());
-            }
-            if (node.has("adminWeight")) {
-                tag.setAdminWeight(node.get("adminWeight").asInt());
+            } else {
+                tag.setWeight(0);
             }
         }
         return tag;
